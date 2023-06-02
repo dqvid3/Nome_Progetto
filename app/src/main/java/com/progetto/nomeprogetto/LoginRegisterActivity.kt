2a0e3d77fc6a0864583ec3d131fbd671d12a24d9
dariolovalvo <dariosyn@gmail.com>
2023-06-02 17:35:45 +0200

    login

--- app/src/main/java/com/progetto/nomeprogetto/LoginRegisterActivity.kt
+++ app/src/main/java/com/progetto/nomeprogetto/LoginRegisterActivity.kt
@@ -8,32 +8,7 @@ import com.progetto.nomeprogetto.Fragments.LoginFragment
 import com.progetto.nomeprogetto.Fragments.RegisterFragment
 import com.progetto.nomeprogetto.databinding.ActivityLoginRegisterBinding
 
-class LoginRegisterActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity()
 
-    private lateinit var binding: ActivityLoginRegisterBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityLoginRegisterBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.loginButton.setOnClickListener{
-            openFragment(LoginFragment())
-            binding.loginButton.visibility = View.INVISIBLE
-            binding.registerButton.visibility = View.INVISIBLE
-        }
-
-        binding.registerButton.setOnClickListener{
-            openFragment(RegisterFragment())
-            binding.loginButton.visibility = View.INVISIBLE
-            binding.registerButton.visibility = View.INVISIBLE
-        }
-    }
-
-    private fun openFragment(fragment: Fragment){
-        val manager = supportFragmentManager
-        val transaction = manager.beginTransaction()
-        transaction.replace(binding.fragmentContainer.id,fragment)
-        transaction.commit()
-    }
-}
