ffc29b9213348624493a26c02118991ee841740d
dqvid3 <davidebonura52@gmail.com>
2023-06-02 18:37:34 +0200

    fixato

--- app/src/main/java/com/progetto/nomeprogetto/Fragments/RegisterFragment.kt
+++ app/src/main/java/com/progetto/nomeprogetto/Fragments/RegisterFragment.kt
@@ -1,19 +1,34 @@
 package com.progetto.nomeprogetto.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.progetto.nomeprogetto.HomeActivity
+import com.progetto.nomeprogetto.LoginRegisterActivity
 import com.progetto.nomeprogetto.R
+import com.progetto.nomeprogetto.databinding.ActivityLoginRegisterBinding
+import com.progetto.nomeprogetto.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater)
+
+        binding.button.setOnClickListener{
+            //se registrazione avvenuta con successo mostra la pagina di login dinuovo
+            //altrimenti mostra toast con errore
+            val i = Intent(requireContext(), LoginRegisterActivity::class.java)
+            startActivity(i)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/progetto/nomeprogetto/LoginRegisterActivity.kt
+++ app/src/main/java/com/progetto/nomeprogetto/LoginRegisterActivity.kt
@@ -1,14 +1,38 @@
 package com.progetto.nomeprogetto
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import com.progetto.nomeprogetto.Fragments.LoginFragment
 import com.progetto.nomeprogetto.Fragments.RegisterFragment
 import com.progetto.nomeprogetto.databinding.ActivityLoginRegisterBinding
 
-class LoginRegisterActivity : AppCompatActivity()
+class LoginRegisterActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginRegisterBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.loginButton.setOnClickListener{
+            //se il login va a buon fine :
+            val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            val editor = sharedPref.edit()
+            editor.putBoolean("IS_LOGGED_IN", true)
+            editor.apply()
+
+            val i = Intent(this,HomeActivity::class.java)
+            startActivity(i)
+        }
+    }
+
+    private fun openFragment(fragment: Fragment){
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(binding.fragmentContainer.id,fragment)
+        transaction.commit()
+    }
+}
